a6dc5fea6aa09cbcd6b6182e441a41d14334c4d4
jayantsapra <67946330+jayantsapra@users.noreply.github.com>
2021-08-04 18:58:37 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,11 +3,11 @@
 session_start();
 
 //Create constants to store Non Repeating Values
-define('SITEURL', 'http://localhost/food-order/');
-define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'food-order');
+define('SITEURL', 'https://food-order-web.herokuapp.com/');
+define('LOCALHOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USERNAME', 'bbbdd980d145aa');
+define('DB_PASSWORD', '35a8f353');
+define('DB_NAME', 'heroku_42cb7f8104668b9');
 
 
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or  die(mysqli_error());//Database connection
